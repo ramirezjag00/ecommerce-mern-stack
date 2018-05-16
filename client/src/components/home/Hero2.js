a280d7c9cb07f9fb78d074fb19a959a443866cfa
Andrey <33412680+ramirezjag00@users.noreply.github.com>
2018-05-16 11:42:55 +0800

    mernV3
    - admin route is only accessible when logged in (used HOC)
    - verification (cancel / proceed) on 'proceed' check out

---
+++ client/src/components/home/Hero2.js
@@ -0,0 +1,20 @@
+ import React, {Component} from 'react';
+import '../app.css';
+
+export default class Hero2 extends Component {
+  render() {
+    return (
+          <div className="hero2-img-container">
+            <div className="hero2-img">
+              <span>WHAT ?</span>
+            </div>
+            <div className="hero2-img">
+              <span>WHY ?</span>
+            </div>
+            <div className="hero2-img">
+              <span>HOW ?</span>
+            </div>
+          </div>
+    );
+  }
+}
